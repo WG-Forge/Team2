22c4304ac7eb94787bc35036d5010df699d48271
thekirjava <thekirjava@yandex.by>
2020-11-20 12:39:05 +0300

    Map update added

--- Map.cpp
+++ Map.cpp
@@ -1,9 +1,22 @@
 #include "Map.h"
+#include "json.h"
+#include <sstream>
 
 Map::Map(const std::string& jsonStructureData, const std::string& jsonCoordinatesData, const std::string& jsonDynamicData):Graph(jsonStructureData, jsonCoordinatesData) {
+	posts.resize(adjacencyList.size(), { Post::PostTypes::NONE, -1, "", -1 });
 	Update(jsonDynamicData);
 }
 
 void Map::Update(const std::string& jsonDynamicData)
 {
+	std::stringstream ss;
+	ss << jsonDynamicData;
+	Json::Document doc = Json::Load(ss);
+	auto nodeMap = doc.GetRoot().AsMap();
+	for (const auto& node : nodeMap["posts"].AsArray()) {
+		auto postMap = node.AsMap();
+		posts[TranslateVertexIdx(postMap["post_idx"].AsInt())] = { static_cast<Post::PostTypes>(postMap["type"].AsInt()), postMap["idx"].AsInt(),
+			postMap["name"].AsString(), postMap["pointIdx"].AsInt() };
+	}
+
 }
