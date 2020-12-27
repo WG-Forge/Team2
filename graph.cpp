b32256eb9deafbbaa4aa699afb4a68259b34cadb
mAyty <pik.paval@gmail.com>
2020-12-27 13:13:44 +0300

    Removed unused code

--- graph.cpp
+++ graph.cpp
@@ -229,9 +229,6 @@ void Graph::DrawEdges(SdlWindow& window) {
 			if (j.to < i) {
 				break;
 			}
-			double k = (maxLength - j.length) / maxLength;
-			k = 0.2 + k * 0.8;
-			unsigned char color = 255 * k;
 			window.SetDrawColor(255, 255, 255);
 			window.DrawLine(std::round(adjacencyList[i].point.x), std::round(adjacencyList[i].point.y), std::round(adjacencyList[j.to].point.x), std::round(adjacencyList[j.to].point.y));
 		}
